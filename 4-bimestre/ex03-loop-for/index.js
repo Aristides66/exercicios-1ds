b246779ddd284aaeeba6192d279f70231d2c7ec6
Aristides <aristides.neto3@aluno.ce.gov.br>
2025-10-14 14:37:10 +0000

    feito!!

--- 4-bimestre/ex03-loop-for/index.js
+++ 4-bimestre/ex03-loop-for/index.js
@@ -1,4 +1,10 @@
 // CRIE UMA LÓGICA AQUI
+let soma = 0
+for(let i = 1; i<=10;i++){
+ soma = soma + i
+ console.log(`${soma}`)
+}
 
+let resultado = soma
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = resultado
