f56070f5654ebe2c6fc361808bdeda1d00dcb0de
Aristides <aristides.neto3@aluno.ce.gov.br>
2025-10-29 23:56:53 +0000

    feito

--- 4-bimestre/ex04-array/index.js
+++ 4-bimestre/ex04-array/index.js
@@ -1,4 +1,7 @@
 // CRIE UMA LÓGICA AQUI
-
+let materias = ["Educação Fisica","Português","Geografia","Espanhol"]
+console.log(materias[0])
+console.log(materias[3])
+console.log(materias.length)
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = { materias }
