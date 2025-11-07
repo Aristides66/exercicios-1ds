4c56473aace0ff4ba3377ea6f01f6bb461b30a1d
Aristides <aristides.neto3@aluno.ce.gov.br>
2025-11-07 19:54:33 +0000

    feito

--- 4-bimestre/ex11-funcao-e-declarando-uma-funcao/index.js
+++ 4-bimestre/ex11-funcao-e-declarando-uma-funcao/index.js
@@ -1,4 +1,12 @@
 // CRIE UMA LÓGICA AQUI
-
+function boasVindas(){
+    return("Bem-vindo(a) ao sistema!")
+}
+function soma(a,b){
+    return a+b
+}
+soma(5,5)
+console.log(`${boasVindas}`)
+console.log(`Resultado da soma: ${soma}`)
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = { boasVindas, soma }
